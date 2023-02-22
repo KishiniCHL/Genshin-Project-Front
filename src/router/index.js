ae074ed38c256e6e7130ed16e1b44289c5d127b5
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-02-22 10:41:01 +0100

    added ayato page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import HutaoView from '../views/HutaoView.vue'
+import AyatoView from '../views/AyatoView.vue'
 
 const routes = [
   {
@@ -16,14 +17,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
-  // {
-  //   path: '/Hutao',
-  //   name: 'Hutao',
-  //   component: HutaoView
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  // }
+  {
+    path: '/Ayato',
+    name: 'Ayato',
+    component: AyatoView
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/AyatoView.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="home" >
+      <div class="containSearch" >
+        <div class="theSearch">
+          <input type="text" placeholder="Search.." class="search" :style="{ border: '4px solid ' + Ayato.colorLighter }">
+        </div>
+      </div>
+      <div class="regionContainer">
+        <RegionPanel :character="Ayato"/>
+      </div>
+      <div class="logoContainer">
+        <LogoName :character="Ayato"/>
+      </div>
+  
+      <div :style="{ backgroundColor: Ayato.color, height:'90vh', width:'100vh', position:'fixed', zIndex:'-2', marginTop:'3rem', borderRadius:'2rem'}"></div>
+      <div>
+        <GalleryComposant :character="Ayato"/>
+      </div>
+      <div :style="{ position:'fixed',left:'130vh', top:'30vh'}">
+        <ColorPalette :character="Ayato"/>
+      </div>
+      
+      <div :style="{ position:'fixed',right:'135vh', top:'10vh'}">
+        <ImagePanel :character="Ayato"/>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import GalleryComposant from '@/components/GalleryComposant'
+  import ColorPalette from '@/components/ColorPalette'
+  import ImagePanel from '@/components/ImagePanel.vue'
+  import LogoName from '@/components/LogoName.vue'
+  import RegionPanel from '@/components/RegionPanel.vue'
+  
+  
+  // @ is an alias to /src
+  export default {
+      name: "AyatoView",
+      components: { GalleryComposant, ColorPalette, ImagePanel, LogoName, RegionPanel },
+      data() {
+      return {
+          Ayato: {
+              name: "Ayato",
+              image: require("@/assets/Kamisato-Ayato-1.webp"),
+              color:"#3F4082",
+              colorLighter:"#696BC0",
+              colorSecondary:"#CFDEF2",
+              colorTertiary:"#7D6BAC",
+              emote1:require("@/assets/Icon_Emoji_065_Hu_Tao_Evil_laugh.webp"),
+              emote2:require("@/assets/Icon_Emoji_070_Hu_Tao_I21.webp"),
+              logoName:require("@/assets/ayatoLogo.png"),
+              region:'inazuma'
+          }
+      }
+    }
+  }
+</script>
+
+
+
+<style>
+
+</style>
